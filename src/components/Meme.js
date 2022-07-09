d0df8ec21ad195c2c44eaa2a28257f4768cf2477
mariajoaosantos <mariajcs18@gmail.com>
2022-07-09 13:26:33 +0100

    Database of meme images added

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,11 +1,31 @@
+import memeData from "../memeData";
+
 export default function Meme() {
+  function getMemeImage() {
+    const memeArray = memeData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memeArray.length);
+    const url = memeArray[randomNumber].url
+    console.log(url)
+  }
+
+
   return (
     <section className="main">
-      <form className="form">
-        <input type="text" placeholder="Top text" className="form--input"></input>
-        <input type="text" placeholder="Bottom text" className="form--input"></input>
-        <button className="form--button">Get a new meme image 🖼️</button>
-      </form>
+      <div className="form">
+        <input
+          type="text"
+          placeholder="Top text"
+          className="form--input"
+        ></input>
+        <input
+          type="text"
+          placeholder="Bottom text"
+          className="form--input"
+        ></input>
+        <button onClick={getMemeImage} className="form--button">
+          Get a new meme image 🖼️
+        </button>
+      </div>
     </section>
   );
 }
