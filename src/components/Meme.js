675c2770502356deb60994029f6befce96511384
mariajoaosantos <mariajcs18@gmail.com>
2022-07-09 15:03:08 +0100

    Included useState to change the images

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,13 +1,28 @@
 import memeData from "../memeData";
+import React from "react";
 
 export default function Meme() {
+  // const [memeImage, setMemeImage] = React.useState(
+  //   "https://i.imgflip.com/30b1gx.jpg"
+  // );
+
+  const [meme, setMeme] = React.useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "https://i.imgflip.com/30b1gx.jpg",
+  });
+
+  const [allMemeImages, setAllMemeImages] = React.useState(memeData);
+
   function getMemeImage() {
     const memeArray = memeData.data.memes;
     const randomNumber = Math.floor(Math.random() * memeArray.length);
-    const url = memeArray[randomNumber].url
-    console.log(url)
+    const url = memeArray[randomNumber].url;
+    setMeme(prevMeme => ({
+      ...prevMeme,
+      randomImage: url
+    }))
   }
-
 
   return (
     <section className="main">
@@ -26,6 +41,7 @@ export default function Meme() {
           Get a new meme image 🖼️
         </button>
       </div>
+      <img src={meme.randomImage} className="meme--image"></img>
     </section>
   );
 }
